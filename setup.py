11b98ba8fc3d0101fadf874f94a4dcd7da17b82a
Davte <testa93davide@gmail.com>
2018-10-23 18:20:52 +0200

    ImportErrors fixed, more information added, typo fixed.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import codecs
 import os
+import re
+import setuptools
 import sys
 
 if sys.version_info < (3,5):
@@ -24,22 +26,20 @@ def find_information(info, *file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
-def setup(**kwargs):
-    for key, val in kwargs.items():
-        print(key, val, sep='\t\t')
-
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
-setup(
+setuptools.setup(
     name='datelepot',
     version=find_information("version", "datelepot", "__init__.py"),
-    author=find_information("version", "datelepot", "__init__.py"),
+    author=find_information("author", "datelepot", "__init__.py"),
     description="telepot.aio.Bot convenient subclass, featuring dataset-powered SQLite.",
+    license=find_information("license", "datelepot", "__init__.py"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/davte/datelepot",
     packages=setuptools.find_packages(),
+    platforms=['any'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Framework :: AsyncIO",
