51b27e2a0d1366b2226c6534227054cb20c4444a
Davte <davte@libero.it>
2018-11-20 19:48:47 +0100

    Stupid me! This code should have never been added to repository. Tokens revoked.

--- davtelepot/__init__.py
+++ davtelepot/__init__.py
@@ -1,8 +1,13 @@
+"""Information about this package.
+
+See custombot.py for information about Bot class.
+"""
+
 __author__ = "Davide Testa"
 __email__ = "davte@libero.it"
 __credits__ = "Marco Origlia"
 __license__ = "GNU General Public License v3.0"
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
