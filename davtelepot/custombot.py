121e3c370c337c0042b837ed661b5a1ecd2d0d7f
Davte <davte@libero.it>
2018-11-20 20:08:06 +0100

    Git-ignore test.py.
    davteutil should be considered third party module since it is homed in a 
    separate repository.

--- davtelepot/custombot.py
+++ davtelepot/custombot.py
@@ -23,15 +23,13 @@ import os
 
 # Third party modules
 import dataset
-import telepot
-import telepot.aio
-
-# Project modules
 from davteutil.utilities import (
     Gettable, escape_html_chars, get_cleaned_text, line_drawing_unordered_list,
     make_lines_of_buttons, markdown_check, MyOD, pick_most_similar_from_list,
     remove_html_tags, sleep_until
 )
+import telepot
+import telepot.aio
 
 
 def split_text_gracefully(text, limit, parse_mode):
