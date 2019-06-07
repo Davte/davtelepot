0bace2824af14afaaf48170fc6aee400c8aebacd
Davte <davte@libero.it>
2019-06-07 21:13:46 +0200

    Version

--- davtelepot/__init__.py
+++ davtelepot/__init__.py
@@ -7,7 +7,7 @@ __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __credits__ = "Marco Origlia"
 __license__ = "GNU General Public License v3.0"
-__version__ = "1.5.5"
+__version__ = "1.5.6"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
