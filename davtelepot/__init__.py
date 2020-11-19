a460f71b697f1dfd2a9718a90feb1e5e01a17807
Davte <davide@davte.it>
2020-11-19 14:37:25 +0100

    Items in __all__ must be str, not module

--- davtelepot/__init__.py
+++ davtelepot/__init__.py
@@ -11,12 +11,12 @@ __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __credits__ = ["Marco Origlia", "Nick Lee @Nickoala"]
 __license__ = "GNU General Public License v3.0"
-__version__ = "2.7.2"
+__version__ = "2.7.3"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
-from . import (administration_tools, api, authorization, bot, helper, languages,
-               messages, suggestions, useful_tools, utilities)
+from . import (administration_tools, api, authorization, bot, helper,
+               languages, messages, suggestions, useful_tools, utilities)
 
-__all__ = [administration_tools, api, authorization, bot, helper, languages,
-           messages, suggestions, useful_tools, utilities]
+__all__ = ['administration_tools', 'api', 'authorization', 'bot', 'helper',
+           'languages', 'messages', 'suggestions', 'useful_tools', 'utilities']
