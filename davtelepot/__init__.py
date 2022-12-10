50f561e7f9322bf89b56277114c73f069014f978
Davte <davide@davte.it>
2022-12-10 18:46:20 +0100

    Method to add a table and its columns to and ` ObjectWithDatabase`

--- davtelepot/__init__.py
+++ davtelepot/__init__.py
@@ -11,7 +11,7 @@ __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __credits__ = ["Marco Origlia", "Nick Lee @Nickoala"]
 __license__ = "GNU General Public License v3.0"
-__version__ = "2.8.8"
+__version__ = "2.8.9"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
